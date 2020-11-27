a870b4a34e493c6e16c6faa780238118617c9238
chungkyungsook <tah05005@gmail.com>
2020-11-27 18:48:56 +0900

    first commit!

--- src/Kyungsook/Routes/Farm.js
+++ src/Kyungsook/Routes/Farm.js
@@ -2,13 +2,15 @@ import React from 'react' ;
 import styled from 'styled-components' ;
 
 const Container = styled.div`
-
+    background: rgb(224, 224, 218,00.3);
+    display: flex;
+    flex-direction: column;
 `;
 
 const Farm = () => {
     return (
         <div>
-            Farm
+
         </div>
     ) ;
 } ;
--- src/Kyungsook/Routes/MyFarm.js
+++ src/Kyungsook/Routes/MyFarm.js
@@ -1,10 +1,26 @@
 import React from 'react' ;
+import '../Css/MyFarm.css';
 
 const MyFarm = () => {
+
+
+    
     return (
-        <div>
-            MyFarm
-        </div>
+        <>
+            <div className="container">
+                <div className = "section">
+                    <div className="item item1">bar</div>
+                    {/* 배지 사진 + 온,습도 */}
+                    <div className="item item2">
+                        <div className =""></div>
+                        <div className =""></div>
+                    </div>
+                    {/* end 배지 사진 + 온,습도 */}
+                    <div className="item item3">item2</div>
+                </div>
+                <div className="footer">footer</div>
+            </div>
+        </>
     );
 };
 
