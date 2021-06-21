034aec538bb64b3d5c97b404f6e961b49a5453af
beomhw <qjatlr6919@gmail.com>
2021-06-21 20:25:01 +0900

    STATE : 로그아웃 차트 수정

--- src/Beomhwan/Components/BaekhwagoTimeline.js
+++ src/Beomhwan/Components/BaekhwagoTimeline.js
@@ -4,7 +4,6 @@ import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 import * as am4timeline from '@amcharts/amcharts4/plugins/timeline';
-import * as am4bullets from '@amcharts/amcharts4/plugins/bullets';
 
 const Container = styled.div`
     width: 100%;
@@ -17,60 +16,107 @@ const BaekhwagoTimeline = () => {
     useLayoutEffect(() => {
         am4core.useTheme(am4themes_animated);
 
-        // setting timeline initial's state
-        const timeline = am4core.create('pyogoTimeline', am4timeline.SerpentineChart);
-        timeline.curveContainer.padding(50, 20, 50, 20);
-        timeline.levelCount = 5;
-        timeline.yAxisRadius = am4core.percent(25);
-        timeline.yAxisInnerRadius = am4core.percent(-25);
-        timeline.maskBullets = false;
-        let title = timeline.titles.create();
+        // Create chart instance
+        var chart = am4core.create("baekhwaTimeline", am4timeline.SpiralChart);
+        chart.levelCount = 3;
+        chart.inversed = true;
+        chart.endAngle = -135;
+
+        let title = chart.titles.create();
         title.text = '백화고 환경 프로그램 로드맵';
         title.fontSize = 20;
 
-        let colorSet = new am4core.ColorSet();
-        colorSet.saturation = 0.5;
-
-        timeline.data = [
-            {
-                "stage": "1단계 : 갓 길이 1cm 이하",
-                "value": "1단계",
-                "state": "갓 길이 1cm 이하"
-            }, {
-                "stage": "2단계 : 갓 길이 2cm ~ 4cm",
-                "value": "1단계",
-                "state": "갓 길이 1cm ~ 2cm"
-            }, {
-                "stage": "3단계 : 갓 길이 2cm ~ 4cm",
-                "value": "1단계",
-                "state": "갓 길이 2cm ~ 4cm"
-            }, {
-                "stage": "4단계 : 갓 길이 4cm ~ 5cm",
-                "value": "1단계",
-                "state": "갓 길이 4cm ~ 5cm"
-            }, {
-                "stage": "5단계 : 갓 길이 5cm 초과",
-                "value": "",
-                "state": "백화고 수확이 가능합니다!"
-            }
+        chart.data = [
+            { 
+                category: "1단계", 
+                openValue: 0, 
+                endValue: 1,
+                description: '1단계에서는 갓 길이가 1cm 이하입니다.'
+            },
+            { 
+                category: "2단계", 
+                openValue: 1, 
+                endValue: 2,
+                description: '2단계에서는 갓 길이가 1cm ~ 2cm 입니다.'
+            },
+            { 
+                category: "3단계", 
+                openValue: 2, 
+                endValue: 3,
+                description: '3단계에서는 갓 길이가 2cm ~ 4cm 입니다.'
+            },
+            { 
+                category: "4단계", 
+                openValue: 3, 
+                endValue: 4,
+                description: '4단계에서는 갓 길이가 4cm ~ 5cm 입니다.'
+            },
+            { 
+                category: "5단계", 
+                openValue: 4, 
+                endValue: 5,
+                description: '5단계에서는 갓 길이가 5cm 초과입니다.'
+            },
         ];
 
-        let categoryAxis = timeline.xAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = 'stage';
-        categoryAxis.renderer.grid.template.disabled = true;
-        categoryAxis.renderer.polyspline.tensionX = 0.8;
-        categoryAxis.renderer.minGridDistance = 30;
-        
-        let valueAxis = timeline.yAxes.push(new am4charts.ValueAxis());
-        valueAxis.renderer.radius = 100;
-        valueAxis.renderer.innerRadius = 0;
-        valueAxis.renderer.grid.template.disabled = true;
+        chart.fontSize = 10;
+        chart.tooltipContainer.fontSize = 10;
 
-        timeLineRef.current = timeline;
-        return () => timeline.dispose();
+        var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "category";
+        categoryAxis.renderer.grid.template.disabled = true;
+        categoryAxis.renderer.minGridDistance = 10;
+        categoryAxis.renderer.innerRadius = 0;
+        categoryAxis.renderer.radius = 80;
+        categoryAxis.cursorTooltipEnabled = false;
+
+
+        var categoryAxisLabelTemplate = categoryAxis.renderer.labels.template;
+        categoryAxisLabelTemplate.paddingLeft = 30;
+        categoryAxisLabelTemplate.horizontalCenter = "left";
+        categoryAxisLabelTemplate.adapter.add("rotation", function (rotation, target) {
+            var position = valueAxis.valueToPosition(valueAxis.min);
+            return valueAxis.renderer.positionToAngle(position) + 90;
+        });
+
+        var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+        valueAxis.renderer.minGridDistance = 70;
+        valueAxis.paddingRight = 50;
+        valueAxis.renderer.grid.template.disabled = true;
+        valueAxis.renderer.line.strokeDasharray = "1,4";
+        valueAxis.renderer.line.strokeOpacity = 0.5;
+        valueAxis.cursorTooltipEnabled = false;
+        valueAxis.min = 0;
+        valueAxis.renderer.labels.template.disabled = true;
+
+        var series = chart.series.push(new am4timeline.CurveColumnSeries());
+        series.dataFields.categoryY = "category";
+        series.dataFields.openValueX = 'openValue';
+        series.dataFields.valueX = 'endValue';
+
+        var columnTemplate = series.columns.template;
+        series.tooltipText = "{description}";
+        series.tooltip.fontSize = 16;
+        columnTemplate.height = am4core.percent(80);
+        columnTemplate.adapter.add("fill", function (fill, target) {
+            return chart.colors.getIndex(target.dataItem.index * 2);
+        })
+        columnTemplate.strokeOpacity = 0;
+        columnTemplate.margin = am4core.percent(30);
+
+        var cursor = new am4timeline.CurveCursor();
+        chart.cursor = cursor;
+        cursor.xAxis = valueAxis;
+        cursor.yAxis = categoryAxis;
+        cursor.lineY.disabled = true;
+        cursor.lineX.strokeDasharray = "1,4";
+        cursor.lineX.strokeOpacity = 1;
+
+        timeLineRef.current = chart;
+        return () => chart.dispose();
     }, []);
 
-    return <Container id="pyogoTimeline" ref={timeLineRef}></Container>
+    return <Container id="baekhwaTimeline" ref={timeLineRef}></Container>
 }
 
 export default BaekhwagoTimeline;
